46ef3f30a6f329768b5c6d04f855c8176b24612f
Gabriella07 <gabil11071996@gmail.com>
2023-05-21 18:03:36 -0400

    solved the FINRA task

--- src/week1_tasks/Gabriela.java
+++ src/week1_tasks/Gabriela.java
@@ -28,11 +28,29 @@ public class Gabriela {
         System.out.println("Remainder: "+num1);
     }
 
+    //task3
+    public static void finra(){
+     for(int i =1; i<=30;i++){
+         if(i%3==0 && i%5==0){
+             System.out.println("FINRA");
+         }else if(i%3==0){
+             System.out.println("FIN");
+         }else if(i%5==0){
+             System.out.println("RA");
+         }else{
+             System.out.println(i);
+         }
+     }
+    }
+
 
     public static void main(String[] args) {
         //task1:
         System.out.println(oddOrEven(7));
         //task2
         dividingWithoutOperator(100,2);
+        //task3
+        finra();
+
     }
 }
