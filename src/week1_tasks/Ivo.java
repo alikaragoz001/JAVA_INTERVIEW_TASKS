ff62e3112af5231f21e4618bb14e579862ed73f4
IvoB29 <tmivong@hotmail.com>
2023-05-22 12:32:41 -0400

    Added task3 finra new changes

--- src/week1_tasks/Ivo.java
+++ src/week1_tasks/Ivo.java
@@ -1,7 +1,7 @@
 package week1_tasks;
 
 public class Ivo {
-// task 1
+    // task 1
     public static String checkEvenOrOdd(int number) {
         if (number % 2 == 0) {
             return "Even";
@@ -10,28 +10,23 @@ public class Ivo {
         }
 
 
-
         // task2
-
-        public static void printFINRA() {
-            for (int i = 1; i <= 30; i++) {
-                if (i % 3 == 0 && i % 5 == 0) {
-                    System.out.print("FINRA ");
-                } else if (i % 3 == 0) {
-                    System.out.print("FIN ");
-                } else if (i % 5 == 0) {
-                    System.out.print("RA ");
-                } else {
-                    System.out.print(i + " ");
-                }
-            }
-        }
-
-
-
-
-
 
 
     }
+
+    public static void printFINRA() {
+        for (int i = 1; i <= 30; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FINRA ");
+            } else if (i % 3 == 0) {
+                System.out.print("FIN ");
+            } else if (i % 5 == 0) {
+                System.out.print("RA ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
 }
