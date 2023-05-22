72572f4efaccf153234e05628c86551a92cb7c77
IvoB29 <tmivong@hotmail.com>
2023-05-22 09:39:19 -0400

    Added Ivo Package and EvenOrOdd task  1 answer

---
+++ src/week1_tasks/Ivo.java
@@ -0,0 +1,14 @@
+package week1_tasks;
+
+public class Ivo {
+// task 1
+    public static String checkEvenOrOdd(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+
+
+    }
+}
