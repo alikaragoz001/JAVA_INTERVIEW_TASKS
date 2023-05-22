7bd876162515476f880a832894c62b654fa22f36
IvoB29 <tmivong@hotmail.com>
2023-05-22 10:03:42 -0400

    Added task3 finra

--- src/week1_tasks/Ivo.java
+++ src/week1_tasks/Ivo.java
@@ -10,5 +10,28 @@ public class Ivo {
         }
 
 
+
+        // task2
+
+        public static void printFINRA() {
+            for (int i = 1; i <= 30; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.print("FINRA ");
+                } else if (i % 3 == 0) {
+                    System.out.print("FIN ");
+                } else if (i % 5 == 0) {
+                    System.out.print("RA ");
+                } else {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+
+
+
+
+
+
+
     }
 }
