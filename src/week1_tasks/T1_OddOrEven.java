79ffa9a3d5f7215a16bb941e91a187b73e922ddc
Cydeo-DI <001dana@gmail.com>
2023-05-22 21:27:23 -0400

    Created Task 1 (OddOrEven) file

---
+++ src/week1_tasks/T1_OddOrEven.java
@@ -0,0 +1,15 @@
+package week1_tasks;
+
+public class T1_OddOrEven {
+
+    public static void main(String[] args) {
+
+    int num = 6;
+
+            if (num % 2 == 0) {
+                System.out.println("Number: " + num + " is even.");
+            } else {
+                System.out.println("Number: " + num + " is odd.");
+            }
+        }
+}
