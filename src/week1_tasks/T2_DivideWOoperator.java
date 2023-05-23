111b80f69693fcba563010e153db4202d6ec9c24
Cydeo-DI <001dana@gmail.com>
2023-05-22 21:56:59 -0400

    Updated Task 2 (Divide without / operator) file

--- src/week1_tasks/T2_DivideWOoperator.java
+++ src/week1_tasks/T2_DivideWOoperator.java
@@ -1,21 +1,16 @@
 package week1_tasks;
 
 public class T2_DivideWOoperator {
-    public static void main(String[] args) {
-     /*   int num1 = 21;
-        int num2 = 7;
+
+    public static void dividingWithoutOperator(int num1, int num2) {
         int result = 0;
 
-        public static int divide(int num1, int num2) {
-            // Handle division by zero
-            if (num2 == 0) {
-                throw new ArithmeticException("Division by zero is not allowed.");
-            }
-            return result;
+        while (num1 >= num2) {
+            num1 = num1 - num2;
+            result++;
         }
-        result = divide(num1, num2);
-        System.out.println("Result: " + result);
 
-*/
-        }
+        System.out.println("Result: " + result);
+        System.out.println("Remainder: " + num1);
     }
+}
