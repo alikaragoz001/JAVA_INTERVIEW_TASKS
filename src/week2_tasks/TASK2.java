3fb9a9f64e539af661490cbb6938f2a7083234ce
IvoB29 <tmivong@hotmail.com>
2023-05-30 12:11:28 -0400

    added task 2

--- src/week2_tasks/TASK2.java
+++ src/week2_tasks/TASK2.java
@@ -1,4 +1,22 @@
 package week2_tasks;
 
 public class TASK2 {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
 }
