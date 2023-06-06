67db4a543b112414aedf92a2b14ba604fe2a864a
MaryKron <marinakronvold@gmail.com>
2023-06-06 12:49:32 -0400

    updated

--- src/week3_tasks/PrimeNumber.java
+++ src/week3_tasks/PrimeNumber.java
@@ -29,3 +29,48 @@ public class PrimeNumber {
 
 
 
+
+/*
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        int number = 17;
+        boolean isPrime = isPrimeNumber(number);
+        System.out.println(number + " is prime? " + isPrime);
+    }
+
+    // Method to check if a number is prime
+    public static boolean isPrimeNumber(int number) {
+        if (number <= 1) {
+            return false;  // Numbers less than or equal to 1 are not prime
+        }
+
+        if (number == 2) {
+            return true;  // 2 is the only even prime number
+        }
+
+        if (number % 2 == 0) {
+            return false;  // Even numbers greater than 2 are not prime
+        }
+
+        // Check divisibility from 3 to the square root of the number (incrementing by 2)
+        for (int i = 3; i * i <= number; i += 2) {
+            if (number % i == 0) {
+                return false;  // The number is divisible, hence not prime
+            }
+        }
+
+        return true;  // The number is not divisible, hence prime
+    }
+}
+
+ */
+
+
+
+
+
+
+
+
+
