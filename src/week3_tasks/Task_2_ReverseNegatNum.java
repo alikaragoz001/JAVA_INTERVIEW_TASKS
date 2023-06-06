bf8c05d4e438b8fde5d8a26e6dae44922bcd8def
Cydeo-DI <001dana@gmail.com>
2023-06-06 11:56:52 -0400

    Created Task 2 of Week 3

---
+++ src/week3_tasks/Task_2_ReverseNegatNum.java
@@ -0,0 +1,24 @@
+package week3_tasks;
+
+public class Task_2_ReverseNegatNum {
+
+    public static void main(String[] args) {
+
+        System.out.println(reverseNum(-780));
+    }
+
+    //    Numbers -- Reverse negative number
+//    Write a return method that can reverse digits of a negative number and return it as int
+//    input: -35  output: -53
+
+public static String reverseNum(int num) {
+
+        String num1 = Integer.toString(num);
+        String result = "";
+
+        for (int i = num1.length() - 1; i >= 1; i--) {
+            result += num1.charAt(i);
+        }
+        return ("-" + result);
+    }
+}
