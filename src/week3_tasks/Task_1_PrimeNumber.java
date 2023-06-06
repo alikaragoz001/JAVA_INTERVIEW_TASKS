5ae9480ec181106938eb90c577244e5486006d16
Cydeo-DI <001dana@gmail.com>
2023-06-06 11:56:41 -0400

    Created Task 1 of Week 3

---
+++ src/week3_tasks/Task_1_PrimeNumber.java
@@ -0,0 +1,34 @@
+package week3_tasks;
+
+import java.util.Scanner;
+
+/*
+Numbers -- Prime Number
+Write a method that can check if a number is prime or not.
+ */
+public class Task_1_PrimeNumber {
+//    public static boolean isPrime(int num){
+//
+////            Scanner scan = new Scanner(System.in);
+////            System.out.println("Please Enter the number: ");
+////            num = scan.nextInt();
+//
+//            int baseNum = 2;
+//            if (num <= 1) {
+//                return false;
+////                System.exit(1);
+//
+//           while(baseNum <= num / 2){
+//               if(num % baseNum == 0){
+//                   return false;
+//               }
+//           baseNum ++;
+//           }
+//                return true;
+//            }
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(isPrime(98));
+//}
+ }
