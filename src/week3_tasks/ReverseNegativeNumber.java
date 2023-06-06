04d6a72b783d6a9696b7c9e8ed61c3c5cb3e3405
MaryKron <marinakronvold@gmail.com>
2023-06-06 12:46:35 -0400

    updated

--- src/week3_tasks/ReverseNegativeNumber.java
+++ src/week3_tasks/ReverseNegativeNumber.java
@@ -6,28 +6,29 @@ input: -35  output: -53
  */
 
     public class ReverseNegativeNumber {
-        public static void main(String[] args) {
-            int number = -35;
-            int reversedNumber = reverseNegativeNumber(number);
-            System.out.println("Reversed number: " + reversedNumber);
-        }
-
-        // Method to reverse the digits of a negative number and return the result as an int
-        public static int reverseNegativeNumber(int number) {
-            boolean isNegative = number < 0;  // Check if the number is negative
-            number = Math.abs(number);  // Convert the number to positive
-
-            int reversedNumber = 0;
-            while (number > 0) {
-                int digit = number % 10;  // Extract the last digit of the number
-                reversedNumber = reversedNumber * 10 + digit;  // Add the digit to the reversed number
-                number /= 10;  // Remove the last digit from the number
+            public static void main(String[] args) {
+                int number = -35;
+                int reversedNumber = reverseNegativeNumber(number);
+                System.out.println("Reversed number: " + reversedNumber);
             }
 
-            // Apply the negative sign if the input number was originally negative
-            return isNegative ? -reversedNumber : reversedNumber;
+            // Method to reverse the digits of a negative number and return it as int
+            public static int reverseNegativeNumber(int number) {
+                boolean isNegative = number < 0;  // Check if the number is negative
+                number = isNegative ? -number : number;  // Convert the number to positive if negative
+
+                int reversedNumber = 0;
+                while (number != 0) {
+                    int digit = number % 10;  // Extract the last digit of the number
+                    reversedNumber = reversedNumber * 10 + digit;  // Add the digit to the reversed number
+                    number = number / 10;  // Remove the last digit from the number
+                }
+
+                return isNegative ? -reversedNumber : reversedNumber;  // Apply the negative sign if the input number was originally negative
+            }
         }
-    }
+
+
 
 
 
