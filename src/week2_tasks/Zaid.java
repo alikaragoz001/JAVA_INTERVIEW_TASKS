48c3a22fea41e560e70b3bd04665d3bff7730a33
zaidalshanableh <zaidalshanableh@gmail.com>
2023-06-06 12:04:50 -0600

    Tasks

--- src/week2_tasks/Zaid.java
+++ src/week2_tasks/Zaid.java
@@ -18,64 +18,6 @@ public class Zaid {
         System.out.println("a = " + a);
         System.out.println("b = " + b);
 
-        System.out.println("----------------------------------");
-
-        // write a program that can print the numbers between 1 ~ 100 that can be divisible by 3,5 and 15
-        // task two
-/*
-        Scanner input =new Scanner(System.in);
-        System.out.println("enter a number");
-        int number = input.nextInt();
-
-            if (number >= 1 && number <= 100) {
-
-                if (number % 3 == 0 && number % 5 == 0 && number % 15 == 0) {
-                    System.out.println("divisibleBy15 = " + number);
-                } else if (number % 5 == 0 ) {
-                    System.out.println("divisibleBy5 = " + number);
-                }else{
-                    System.out.println("divisibleBy3 = " + number);
-                }
-            }
-
-        input.close();
-            */
-
-
-        System.out.println("------------------------------------");
-
-        // task # 3
-        int N = 24;
-        printNumbers(N);
-
-
 
     }
-
-    public static void printNumbers(int N) {
-
-        for (int i = 1; i <= N; i++) {
-
-            if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0) {
-                System.out.println("CodilityTestCoders");
-            } else if (i % 2 == 0 && i % 3 == 0) {
-                System.out.println("CodilityTest");
-            } else if (i % 2 == 0 && i % 5 == 0) {
-                System.out.println("CodilityCoders");
-            } else if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("testCoders");
-            } else if (i % 2 == 0) {
-                System.out.println("Codility");
-            } else if (i % 3 == 0) {
-                System.out.println("Test");
-            } else if (i % 5 == 0) {
-                System.out.println("Coders");
-            } else {
-                System.out.println(i);
-            }
-        }
-    }
-
-
-
 }
---
+++ src/week2_tasks/Zaid3.java
@@ -0,0 +1,39 @@
+package week2_tasks;
+
+public class Zaid3 {
+    public static void main(String[] args) {
+
+
+        // task # 3
+        int N = 24;
+        printNumbers(N);
+
+    }
+
+
+
+    public static void printNumbers(int N) {
+
+        for (int i = 1; i <= N; i++) {
+
+            if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0) {
+                System.out.println("CodilityTestCoders");
+            } else if (i % 2 == 0 && i % 3 == 0) {
+                System.out.println("CodilityTest");
+            } else if (i % 2 == 0 && i % 5 == 0) {
+                System.out.println("CodilityCoders");
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("testCoders");
+            } else if (i % 2 == 0) {
+                System.out.println("Codility");
+            } else if (i % 3 == 0) {
+                System.out.println("Test");
+            } else if (i % 5 == 0) {
+                System.out.println("Coders");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
