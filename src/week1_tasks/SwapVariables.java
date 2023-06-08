3e4a06918cbbd14dd3135fdfc28651a359a98603
irynapapkova <skinbyirene@yahoo.com>
2023-06-08 19:03:59 -0400

    Swap Variables

---
+++ src/week1_tasks/SwapVariables.java
@@ -0,0 +1,23 @@
+package week1_tasks;
+
+public class SwapVariables {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        // Swapping without using a third variable
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+    }
+}
