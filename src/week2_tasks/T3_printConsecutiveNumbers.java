4543c2ebc39d6ebeaddbb2ede78d87feafd61529
irynapapkova <skinbyirene@yahoo.com>
2023-06-27 12:57:15 -0400

    task completed

---
+++ src/week2_tasks/T3_printConsecutiveNumbers.java
@@ -0,0 +1,30 @@
+package week2_tasks;
+
+public class T3_printConsecutiveNumbers {
+    public static void printNums (int N) {
+        for (int i = 1; i <= N; i++) {
+            String result = "";
+
+            if (i % 2 == 0) {
+                result += "Codility";
+            }
+            if (i % 3 == 0){
+                result += "Test";
+            }
+            if (i % 5 == 0) {
+                result += "Coders";
+            }
+            if (result.equals("")) {
+                System.out.println(i);
+            }else {
+                System.out.println(result);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int N = 24;
+        printNums(N);
+    }
+}
