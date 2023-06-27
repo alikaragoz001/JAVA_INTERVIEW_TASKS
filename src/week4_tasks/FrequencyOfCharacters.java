8ffde79eac84266439dfeecaf85a0354c4647600
irynapapkova <skinbyirene@yahoo.com>
2023-06-26 20:57:53 -0400

    frequency of characters

---
+++ src/week4_tasks/FrequencyOfCharacters.java
@@ -0,0 +1,23 @@
+package week4_tasks;
+
+public class FrequencyOfCharacters {
+    public static String frequencyOfChars (String str ) {
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            int count = 1;
+            while (i + 1 < str.length() && str.charAt(i + 1) == ch ) {
+                count++;
+                i++;
+            }
+            result.append(ch).append(count);
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(frequencyOfChars("AAABBCDD"));
+    }
+}
