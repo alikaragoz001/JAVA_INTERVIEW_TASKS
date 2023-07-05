d77a7ad42dd48ba629a20c2a4d364df6f108bcd9
MaryKron <marinakronvold@gmail.com>
2023-07-05 18:48:40 -0400

    T3 update

--- src/week5_tasks/T2_StringReverse.java
+++ src/week5_tasks/T2_StringReverse.java
@@ -12,7 +12,6 @@ public class T2_StringReverse {
         int lnt = string.length();                     // getting the length of the string
 
         for (int i = lnt - 1; i >= 0; i--) {            // iterate through the characters of the input string in reverse order
-
             reversed = reversed + string.charAt(i);      // append each character to the reversed string
         }
         return reversed;
@@ -20,11 +19,11 @@ public class T2_StringReverse {
 
     public static void main(String[] args) {
 
+
         System.out.println("reverseString(\"ABCD\") = " + reverseString("ABCD"));
 
 
         StringBuffer easyReverse = new StringBuffer("ABCD");
-
         System.out.println("Reverse using StringBuffer = "  + easyReverse.reverse());
 
     }
