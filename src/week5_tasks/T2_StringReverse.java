dcc0d2e682d5cea9f61361ed2ab0109b3c562407
MaryKron <marinakronvold@gmail.com>
2023-07-05 17:10:41 -0400

    T3 update

--- src/week5_tasks/T2_StringReverse.java
+++ src/week5_tasks/T2_StringReverse.java
@@ -5,24 +5,27 @@ Write a return method that can reverse String Ex: Reverse("ABCD"); ==> DCBA
  */
 public class T2_StringReverse {
 
+    public static String reverseString(String string) {
+
+        String reversed = "";                          // creating an empty string to store the reversed string
+
+        int lnt = string.length();                     // getting the length of the string
+
+        for (int i = lnt - 1; i >= 0; i--) {            // iterate through the characters of the input string in reverse order
+
+            reversed = reversed + string.charAt(i);      // append each character to the reversed string
+        }
+        return reversed;
+    }
+
     public static void main(String[] args) {
 
-       String s = "ABCD";
-
-        int lnt = s.length();  // getting the length of the string
-        String rev = "";       // creating an empty string to store the reversed string
-
-        for (int i = lnt - 1; i >= 0; i--) {   // iterate through the characters of the input string in reverse order
-
-            rev = rev + s.charAt(i);    // append each character to the reversed string
-
-        }
-
-        System.out.println(rev);
+        System.out.println("reverseString(\"ABCD\") = " + reverseString("ABCD"));
 
 
-        //   StringBuffer sf = new StringBuffer(s);
-        //  System.out.println(sf.reverse());
+        StringBuffer easyReverse = new StringBuffer("ABCD");
+
+        System.out.println("Reverse using StringBuffer = "  + easyReverse.reverse());
 
     }
 
