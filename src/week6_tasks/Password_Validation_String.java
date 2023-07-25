098a404b1a9c26ebcd604d652def7a2fe9fd43d3
MaryKron <marinakronvold@gmail.com>
2023-07-25 15:48:45 -0400

    Validation password

--- src/week6_tasks/Password_Validation_String.java
+++ src/week6_tasks/Password_Validation_String.java
@@ -7,7 +7,47 @@ String -- Password Validation Task
 4. Password should at least contain one special characters
 5. Password should at least contain a digit
 if all requirements above are met, the method returns true, otherwise returns false
-Array -- Find Maximum
+
  */
 public class Password_Validation_String {
-}
+
+        public static void main(String[] args) {
+            String password = "Abc123@";
+            boolean isValid = isValidPassword(password);
+            System.out.println("Is the password valid? " + isValid);
+        }
+
+        public static boolean isValidPassword(String password) {
+            // Check if password length is at least 6 characters and does not contain space
+            if (password.length() < 6 || password.contains(" ")) {
+                return false;
+            }
+
+            // Check if password contains at least one uppercase letter
+            if (!password.matches(".*[A-Z].*")) {
+                return false;
+            }
+
+            // Check if password contains at least one lowercase letter
+            if (!password.matches(".*[a-z].*")) {
+                return false;
+            }
+
+            // Check if password contains at least one special character
+            // Special characters are defined using a regular expression pattern [!@#$%^&*(),.?\":{}|<>]
+            if (!password.matches(".*[!@#$%^&*(),.?\":{}|<>].*")) {
+                return false;
+            }
+
+            // Check if password contains at least one digit
+            if (!password.matches(".*\\d.*")) {
+                return false;
+            }
+
+            // If all requirements are met, the password is valid
+            return true;
+        }
+    }
+
+
+
